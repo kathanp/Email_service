e97624b65dcfcde7540916d1ba5f15e60cb287b3
Kathan Patel <patelkathan244@gmail.com>
2025-07-09 09:35:14 -0500

    Fix useRef import error in CustomerManager.js

--- client/src/pages/CustomerManager.js
+++ client/src/pages/CustomerManager.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { API_ENDPOINTS } from '../config';
 import './CustomerManager.css';
 import { useNavigate } from 'react-router-dom';
